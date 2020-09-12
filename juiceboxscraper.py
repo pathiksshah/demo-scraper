33fabae07195ac6ef48f0b8fc71bb56e5b39a817
Pathik.shah <user@gmail.com>
2020-09-12 13:54:06 -0700

    Revert "Initialized the scraper"
    This reverts commit 536fdc25782bf26e1acdaf96b0e8461f30981924.

--- juiceboxscraper.py
+++ juiceboxscraper.py
@@ -4,18 +4,7 @@ import requests
 import re
 
 def main():
-
-    source = requests.get(f'https://juicebox.sec-demo.com/#/').text
-    soup = BeautifulSoup(source, 'lxml')
-    print(soup)
-    # pattern = re.compile(r'List Price:\s(\$[\d]{1,3}(\.[\d]{2})?)')
-    # print(f'Scraping {item} inventory......\n')
-    # productsplus = soup.find('table',id = 'tblContent')
-    # products = productsplus.find_all('td',class_='c4')
-    # for product in products:
-    #     name = product.a.text.strip()
-    #     avail = product.span.text
-    #     price = pattern.search(str(product)).group(1)
-    #     print (f'{item}: {name}\nPrice: {price}\nAvailability: {avail}\n')
+    pass
+    print("work in progress")
 main()
     
